7fbec03ac5c6bc0baee39797c64b5ea8c97bdaab
Sebastian Walter <sebwalter@gmx.net>
2019-10-10 22:45:59 +0200

    PackIconRPGAwesome

---
+++ EvilBaschdi.CoreExtended/Converter/PackIconRPGAwesomeImageSourceConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media;
+using MahApps.Metro.IconPacks;
+
+namespace EvilBaschdi.CoreExtended.Converter
+{
+    /// <summary>
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public class PackIconRPGAwesomeImageSourceConverter : PackIconImageSourceConverterBase<PackIconRPGAwesomeKind>
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="foregroundBrush"></param>
+        /// <param name="penThickness"></param>
+        /// <returns></returns>
+        protected override ImageSource CreateImageSource(object value, Brush foregroundBrush, double penThickness)
+        {
+            var packIcon = new PackIconRPGAwesome
+                           {
+                               Kind = (PackIconRPGAwesomeKind) value
+                           };
+
+            return InnerCreateImageSource(foregroundBrush, penThickness, packIcon.Data);
+        }
+    }
+}

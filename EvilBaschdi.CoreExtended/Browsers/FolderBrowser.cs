4308c18558bfb777e225312970b2f8cbbed924fa
Sebastian Walter <sebwalter@gmx.net>
2019-10-15 08:20:15 +0200

    removed windows forms classes

--- EvilBaschdi.CoreExtended/Browsers/FolderBrowser.cs
+++ EvilBaschdi.CoreExtended/Browsers/FolderBrowser.cs
@@ -1,45 +1,45 @@
-﻿using System.Windows.Forms;
+﻿//using System.Windows.Forms;
 
-namespace EvilBaschdi.CoreExtended.Browsers
-{
-    /// <inheritdoc />
-    /// <summary>
-    ///     Class for FolderBrowser.
-    /// </summary>
-    public class FolderBrowser : IFolderBrowser
-    {
-        private string _selectedPath;
+//namespace EvilBaschdi.CoreExtended.Browsers
+//{
+//    /// <inheritdoc />
+//    /// <summary>
+//    ///     Class for FolderBrowser.
+//    /// </summary>
+//    public class FolderBrowser : IFolderBrowser
+//    {
+//        private string _selectedPath;
 
-        /// <inheritdoc />
-        /// <summary>
-        ///     Shows FolderBrowser.
-        /// </summary>
-        public void ShowDialog()
-        {
-            var folderDialog = new FolderBrowserDialog
-                               {
-                                   SelectedPath = _selectedPath
-                               };
+//        /// <inheritdoc />
+//        /// <summary>
+//        ///     Shows FolderBrowser.
+//        /// </summary>
+//        public void ShowDialog()
+//        {
+//            var folderDialog = new FolderBrowserDialog
+//                               {
+//                                   SelectedPath = _selectedPath
+//                               };
 
-            var result = folderDialog.ShowDialog();
-            if (result.ToString() != "OK")
-            {
-                return;
-            }
+//            var result = folderDialog.ShowDialog();
+//            if (result.ToString() != "OK")
+//            {
+//                return;
+//            }
 
-            _selectedPath = folderDialog.SelectedPath;
-        }
+//            _selectedPath = folderDialog.SelectedPath;
+//        }
 
-        /// <inheritdoc />
-        /// <summary>
-        ///     Get or Set selected path.
-        /// </summary>
-        public string SelectedPath
-        {
-            get => string.IsNullOrWhiteSpace(_selectedPath)
-                ? string.Empty
-                : _selectedPath;
-            set => _selectedPath = value;
-        }
-    }
-}
+//        /// <inheritdoc />
+//        /// <summary>
+//        ///     Get or Set selected path.
+//        /// </summary>
+//        public string SelectedPath
+//        {
+//            get => string.IsNullOrWhiteSpace(_selectedPath)
+//                ? string.Empty
+//                : _selectedPath;
+//            set => _selectedPath = value;
+//        }
+//    }
+//}

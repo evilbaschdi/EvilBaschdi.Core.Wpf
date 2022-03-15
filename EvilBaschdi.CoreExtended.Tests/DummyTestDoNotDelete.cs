51e34fd3a19572e4917cfbcbad4d1b3eeee85ecb
Sebastian Walter <sebwalter@gmx.net>
2022-03-15 21:41:43 +0100

    updated nuget packages

---
+++ EvilBaschdi.CoreExtended.Tests/DummyTestDoNotDelete.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace EvilBaschdi.CoreExtended.Tests
+{
+    /// <summary>
+    ///     Do not delete this class.
+    ///     NCrunch needs at least one test method in witch <see cref="Assert" />.Equal() is used.
+    /// </summary>
+    public class DummyTestDoNotDelete
+    {
+        [Fact]
+        public void Value_ToEnableUnitTests_Asserts1Equals1()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Equal(1, 1);
+        }
+    }
+}

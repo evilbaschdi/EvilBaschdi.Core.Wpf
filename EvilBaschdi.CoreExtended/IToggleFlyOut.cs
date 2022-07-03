9e4a2a5ca388b0471d282a37ba8d7ea6e5306728
Sebastian Walter <sebwalter@gmx.net>
2022-07-03 11:25:13 +0200

    added ToggleFlyOut

---
+++ EvilBaschdi.CoreExtended/IToggleFlyOut.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using MahApps.Metro.Controls;
+
+namespace EvilBaschdi.CoreExtended;
+
+/// <summary>
+/// </summary>
+public interface IToggleFlyOut
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="flyOuts"></param>
+    /// <param name="index"></param>
+    /// <param name="stayOpen"></param>
+    void RunFor([CanBeNull] FlyoutsControl flyOuts, int index, bool stayOpen);
+}

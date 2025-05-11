6a8be6971ead20131e327659f8d80cd9952db0cf
Sebastian Walter <sebwalter@gmx.net>
2025-05-11 20:15:17 +0200

    Resolved conflicts caused by updating EvilBaschdi.Core

--- EvilBaschdi.Core.Wpf/Mvvm/ViewModel/Command/IDefaultCommandRunAsync.cs
+++ EvilBaschdi.Core.Wpf/Mvvm/ViewModel/Command/IDefaultCommandRunAsync.cs
@@ -1,6 +1,6 @@
 ﻿namespace EvilBaschdi.Core.Wpf.Mvvm.ViewModel.Command;
 
 /// <inheritdoc cref="IDefaultCommand" />
-/// <inheritdoc cref="ITaskFromMethod" />
+/// <inheritdoc cref="ITaskRun" />
 // ReSharper disable once UnusedType.Global
-public interface IDefaultCommandRunAsync : IDefaultCommand, ITaskFromMethod;
+public interface IDefaultCommandRunAsync : IDefaultCommand, ITaskRun;
